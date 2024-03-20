905d3a3ca1687cd81664a471d7236cf88860b12d
Matjaž <35506203+matek100@users.noreply.github.com>
2024-03-20 16:33:09 +0100

    Create hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+basic.show_number(1000) #Prints out a number
+basic.pause(500) #500ms pause
+basic.show_string("Hello wrold!") #Prints out a string
+basic.pause(500)
+basic.show_leds("""
+. . # . .
+# . . . #
+. # . # .
+. . # . .
+. . . . .
+""") # LED light that is . - off, # - on 
+basic.pause(1000)
+basic.clear_screen() #clears screen
