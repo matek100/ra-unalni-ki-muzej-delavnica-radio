1c906848d4f8484b614e17b51f92e32e5e969db4
Matjaž <35506203+matek100@users.noreply.github.com>
2024-03-21 21:01:26 +0100

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,13 +1,17 @@
-basic.show_number(1000) #Prints out a number
-basic.pause(500) #500ms pause
-basic.show_string("Hello wrold!") #Prints out a string
-basic.pause(500)
-basic.show_leds("""
-. . # . .
-# . . . #
-. # . # .
-. . # . .
-. . . . .
-""") # LED light that is . - off, # - on 
-basic.pause(1000)
-basic.clear_screen() #clears screen
+#Imports
+from microbit import *
+import radio
+
+display.show("hello world!") #Displays a string, character by character
+sleep(500) #waits for 500ms
+display.clear() #clears screan
+display.show(123)#displays an integer
+sleep(500)
+display.scroll("Hello museum!") #Displays a string, but the text scrolls by
+display.show(Image('00300:' #Sets brightness of each individual LED light, 0 - off, 9 - full brightness 
+                   '03630:'
+                   '36963:'
+                   '03630:'
+                   '00300'))
+sleep(600)
+display.clear()
